32e0fe408066b160a92af01a418535c093e3e9c1
kanno <812137533@qq.com>
2022-11-16 11:56:18 +0800

    feat: support load esm package (#4)

--- src/shared.ts
+++ src/shared.ts
@@ -1,3 +1,5 @@
+import path from 'path'
+import fs from 'fs'
 import { createRequire } from 'module'
 import type { InternalError } from './interface'
 
@@ -9,6 +11,8 @@ import type { InternalError } from './interface'
 // @ts-ignored
 const _require = createRequire(import.meta.url)
 
+export const requireResolve = (module: string): string => _require.resolve(module)
+
 export const tryRequireModule = <T>(module: string): T => {
   return _require(module) as T
 }
@@ -16,10 +20,23 @@ export const tryRequireModule = <T>(module: string): T => {
 export const ERRORS = {
   INVALID_PACKAGE: 'INVALID_PACKAGE',
   NO_PRESET_FIELDS: 'NO_PRESET_FIELDS',
-  INVALID_PRESET: 'INVALID_PRESET'
+  INVALID_PRESET: 'INVALID_PRESET',
+  ERR_PACKAGE_PATH_NOT_EXPORTED: 'ERR_PACKAGE_PATH_NOT_EXPORTED'
 } as const
 
 export const error = (native: Error | InternalError) => {
   if (!(native instanceof Error)) native = Object.assign(new Error(native.message), native)
   throw native
+}
+
+export const lookup = (entry: string, target: string): string => {
+  const dir = path.dirname(entry)
+  const targetFile = path.join(dir, target)
+  if (fs.existsSync(targetFile)) return targetFile
+  return lookup(dir, target)
+}
+
+export const tryRequireRealModule = <T>(module: string): T => {
+  const str = fs.readFileSync(module, 'utf8')
+  return JSON.parse(str)
 }
