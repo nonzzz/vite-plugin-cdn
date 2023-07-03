6da161fdf9034a08deda0e5f141724d1e6696ebc
kanno <812137533@qq.com>
2023-07-03 11:08:25 +0800

    feat: init code-gen

---
+++ __tests__/code-gen.spec.ts
@@ -0,0 +1,44 @@
+import test from 'ava'
+import { createCodeGenerator } from '../src/code-gen'
+import { createScanner } from '../dist/scanner'
+import type { ModuleInfo } from '../src/interface'
+
+
+test('filter', async (t) => {
+  const code = `
+     import { ref } from 'vue';
+     const v = ref(0)
+    `
+  const dependencies:Map<string, ModuleInfo> = new Map()
+  const codeGen = createCodeGenerator()
+  dependencies.set('vue', {
+    name: 'vue',
+    global: 'Vue',
+    version: '0.0.0',
+    relativeModule: '',
+    bindings: new Set()
+  })
+  codeGen.injectDependencies(dependencies)
+  t.is(codeGen.filter(code, 'mock.js'), true)
+})
+
+test('transform', async (t) => {
+  const code = `
+   import { ref } from 'vue';
+   function useState (val) {
+     const state = ref(val);
+     const dispatch = (next) => {
+        state.value =  typeof next === 'function' ? next(state.value) : next;
+     }
+     return [state,dispatch];
+   }
+   const [visible,setVisible] = useState(false);
+  `
+  const scanner = createScanner(['vue'])
+  await scanner.scanAllDependencies()
+  const codeGen = createCodeGenerator()
+  codeGen.injectDependencies(scanner.dependencies)
+  //   const res = await codeGen.transform(code)
+  //   t.is(res.code, '')
+  t.pass()
+})
---
+++ src/code-gen.ts
@@ -0,0 +1,41 @@
+import { parse as esModuleLexer } from 'rs-module-lexer'
+import { parse as babelParse, traverse, transformFromAstAsync } from '@babel/core'
+import { len } from './shared'
+import type { IIFEModuleInfo } from './interface'
+
+export class CodeGen {
+  private dependencies:Map<string, IIFEModuleInfo>
+  injectDependencies(dependencies:Map<string, IIFEModuleInfo>) {
+    this.dependencies = dependencies
+  }
+
+  filter(code:string, id:string) {
+    const { output } = esModuleLexer({ input: [{ filename: id, code }] })
+    if (!len(output)) return false
+    const { imports } = output[0]
+    const modules = Array.from(new Set([...imports.map(i => i.n)]))
+    for (const m of modules) {
+      if (this.dependencies.has(m)) return true
+      return false
+    }
+    return false
+  }
+
+  async transform(code:string) {
+    const ast = await babelParse(code)
+    traverse(ast, {
+      // 
+    })
+    const result = await transformFromAstAsync(ast)
+    return {
+      code: result.code,
+      map: result.map
+    }
+  }
+}
+
+
+export function createCodeGenerator() {
+  return new CodeGen() 
+}
+
