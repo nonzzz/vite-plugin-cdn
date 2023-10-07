826b2e7e56a8182ba53ffdb4d943ea3a0bb96a9b
kanno <812137533@qq.com>
2023-10-07 16:38:12 +0800

    test: make test happy

---
+++ __tests__/fixtures/cdn-plugin/main.js
@@ -0,0 +1,15 @@
+/* eslint-disable */ 
+import { createApp, createVNode, defineComponent } from 'vue'
+
+const App = defineComponent({
+  name: 'Application',
+  setup() {
+    return () => createVNode('div', null, 'hello world')
+  }
+})
+
+createApp(App).mount('#app')
+
+export * from 'vue'
+const version = 'cdn-plugin-test-version'
+export { version }
